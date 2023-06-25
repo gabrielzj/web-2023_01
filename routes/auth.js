b94cafc51f70382b3fe712b9f6d949f79dc1e227
Gabriel Zuin <gabrielzuinj@gmail.com>
2023-06-25 18:26:29 -0300

    Rout /login and /signup

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,24 @@ const Router = express.Router()
 /**
  * Existe uma rota /auth
  */
-//rota login
+//rota login > rendereiza template de login
 Router.get('/login', (req, res) => {
-    res.render('index')
+    res.render('login')
     // console.log('esta dentro de /auth/login')
     // console.log(req.body.email, req.body.password)
 })
-//rota cadastro
+//rota cadastro > renderiza template de cadastro
 Router.get('/signup', (req, res) =>{
     res.render('cadastro')
 })
+
+Router.post('/login', (req, res) =>{
+    res.send('Email:'+req.body.LoginEmail+'\tSenha:'+req.body.LoginSenha)
+})
+
+Router.post('/signup', (req, res) =>{
+    res.send('Nome:' + req.body.CadNome + '\tEmail:' +req.body.CadEmail + 'Senha:' + req.body.CadSenha)
+})
+
 
 module.exports = Router
