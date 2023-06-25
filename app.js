2a01dc0a9cfb391c26f38abc81cb8a62a764c3ef
Gabriel Zuin <gabrielzuinj@gmail.com>
2023-06-24 22:53:35 -0300

    add body-parser dependence

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 
-const indexRouter = require('./routes/index');
+
 const authRouter = require('./routes/auth')
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users')
 
 const app = express();
 
-const mustacheExpress = require("mustache-express");
-const engine = mustacheExpress();
+const mustacheExpress = require("mustache-express")
+const engine = mustacheExpress()
 const PORT = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-app.engine("mustache", engine);
-app.set('view engine', 'mustache');
+app.engine("mustache", engine)
+app.set('view engine', 'mustache')
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger('dev'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public'))); //server pra reconhecer arquivos estaticos
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use('/auth', authRouter)
 //Rota que fazem parte de /users
-app.use('/users', usersRouter);
+app.use('/users', usersRouter)
+// app.use('/auth', bodyParser)
 
 // se nao for nenhuma rota acima, gerar um erro
 app.use(function(req, res, next) {
   // TODO gerar uma pagina 404
-  res.status(404).send('Nao foi possivel encontrar essa rota.');
+  res.status(404).send('Nao foi possivel encontrar essa rota.')
 });
 //especifica a porta utilizada
 app.listen(PORT, () => {
-  console.log(`Running in http://localhost:${PORT}`);
+  console.log(`Running in http://localhost:${PORT}`)
 })
 
 module.exports = app;
