422edf380f50de71f554ba1421c08ac819522175
Gabriel Zuin <gabrielzuinj@gmail.com>
2023-07-05 01:12:39 -0300

    update index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const Router = express.Router()
+const path = require('path')
 
 //rota raiz
 Router.get('/', (req, res) => {
-    res.send('<p>Bem vindo</p>')
+    res.send('<p>Servidor esta rodando...</p>')
 })
 
 module.exports = Router
